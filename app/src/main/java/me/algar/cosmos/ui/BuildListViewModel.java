10c579c6cbd60965e7d88c47395d1262796cfb1a
david.algar <david@algar.me>
2016-03-12 18:38:59 -0800

    add new models

---
+++ app/src/main/java/me/algar/cosmos/ui/BuildListViewModel.java
@@ -0,0 +1,23 @@
+package me.algar.cosmos.ui;
+
+import me.algar.cosmos.api.JenkinsRequestManager;
+import me.algar.cosmos.api.models.Job;
+import rx.subjects.AsyncSubject;
+
+public class BuildListViewModel {
+    private final AsyncSubject<Job> buildDataSubject;
+    private JenkinsRequestManager requestManager;
+
+    public BuildListViewModel(JenkinsRequestManager manager){
+        requestManager = manager;
+        this.buildDataSubject = AsyncSubject.create();
+    }
+
+    public AsyncSubject<Job> getBuildDataSubject() {
+        return buildDataSubject;
+    }
+
+    public void loadBuilds(int startIndex){
+        requestManager.getJob("Cosmos%20-%20development", startIndex).subscribe(buildDataSubject);
+    }
+}
---
+++ app/src/main/java/me/algar/cosmos/ui/JobListViewModel.java
@@ -0,0 +1,7 @@
+package me.algar.cosmos.ui;
+
+/**
+ * Created by david.algar on 3/12/16.
+ */
+public class JobListViewModel {
+}
